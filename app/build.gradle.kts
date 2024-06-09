06b27e8cbd13c8fe7a6265afdc3a9fd905622ea4
amrbashir <amr.bashir2015@gmail.com>
2024-06-09 04:28:44 +0300

    chore: add ktlintFormat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,23 +63,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("com.google.android.material:material:1.12.0")
-
     implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("androidx.activity:activity-compose:1.9.0")
-
-    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
-
     implementation("androidx.glance:glance-appwidget:1.0.0")
     implementation("androidx.glance:glance-material3:1.0.0")
-
     implementation("androidx.navigation:navigation-compose:2.7.7")
-
-    implementation("io.ktor:ktor-client-core:2.3.10")
-    implementation("io.ktor:ktor-client-android:2.3.10")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.10")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.10")
-
     implementation("com.github.skydoves:colorpicker-compose:1.0.8")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,18 @@
 plugins {
     id("com.android.application") version "8.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1" apply false
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        filter {
+            exclude("*.gradle.kts")
+            exclude {
+                it.file.path.contains("${layout.buildDirectory}/generated/")
+            }
+        }
+    }
 }
