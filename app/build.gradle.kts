a25dec65f201408f42c48f41dce20541c870da2c
amrbashir <github@amrbashir.me>
2024-09-30 05:14:52 +0300

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "me.amrbashir.hijriwidget"
-    compileSdk = 34
+  compileSdk = 35
 
-    defaultConfig {
+  defaultConfig {
         applicationId = "me.amrbashir.hijriwidget"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 8
         versionName = "0.8.0"
     }
@@ -59,16 +59,16 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.09.02"))
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.material3:material3:1.3.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    implementation("androidx.glance:glance-appwidget:1.0.0")
-    implementation("androidx.glance:glance-material3:1.0.0")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.glance:glance-appwidget:1.1.0")
+    implementation("androidx.glance:glance-material3:1.1.0")
+    implementation("androidx.navigation:navigation-compose:2.8.1")
     implementation("com.github.skydoves:colorpicker-compose:1.0.8")
 }
