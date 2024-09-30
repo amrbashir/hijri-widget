521e884c84fbcf44e9b37e2cf70bb4b0de330e19
amrbashir <github@amrbashir.me>
2024-09-30 05:37:41 +0300

    release: 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.amrbashir.hijriwidget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.8.0"
+        versionCode = 9
+        versionName = "0.9.0"
     }
 
     signingConfigs {
