978e664ac28a7def35ba83b8d62169dd25051a06
amrbashir <github@amrbashir.me>
2025-08-24 03:17:47 +0300

    feat: add rounded corners to PreferenceButton

--- app/src/main/kotlin/me/amrbashir/hijriwidget/preferences/composables/ui/PreferenceButton.kt
+++ app/src/main/kotlin/me/amrbashir/hijriwidget/preferences/composables/ui/PreferenceButton.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalTextStyle
@@ -19,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
@@ -42,6 +44,7 @@ fun PreferenceButton(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = modifier
             .addIf(onClick != null) { clickable(enabled, onClick = onClick!!) }
+            .clip(RoundedCornerShape(4.dp))
             .background(MaterialTheme.colorScheme.surfaceBright)
             .padding(16.dp)
     ) {
